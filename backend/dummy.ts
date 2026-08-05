518172272ca31c81a403ae1f34533466c5775540
Rymaxw <jm.jurrymaxwell@gmail.com>
2026-08-05 15:13:28 +0700

    ready to deploy

--- backend/dummy.ts
+++
@@ -1,4 +0,0 @@
-import express, { Request, Response } from 'react';
-// Wait, express from 'express'.
-import expressApp from 'express';
-// I will write the proper code without importing from react.
